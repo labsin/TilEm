c1c77416f9766b322a7202838fdbe8d18ef82e6b
floppusmaximus <floppusmaximus@05cf5367-e1a8-405a-8a5c-7f956f11530a>
2010-01-14 05:48:09 +0000

    Cleanup of linking code. Use MODE_SEND_LAST_VAR (i.e., send EOT) when appropriate. Allow sending backup and certificate files.
    git-svn-id: svn://svn.code.sf.net/p/tilem/code/trunk@84 05cf5367-e1a8-405a-8a5c-7f956f11530a

--- gui-qt/calclink.cpp
+++ gui-qt/calclink.cpp
@@ -33,7 +33,7 @@
 static int get_calc_model(TilemCalc* calc);
 static CableHandle* external_link_handle_new();
 static CableHandle* internal_link_handle_new(Calc *calc);
-static void send_file(CalcHandle* ch, int first, int last, const char* filename);
+static int send_file(CalcHandle* ch, int last, const char* filename);
 #endif
 
 /*!
@@ -83,7 +83,8 @@ class FileSender : public QThread
 				// TODO : first wait for any exchange using direct connection to end...
 				m_link->m_calc->setBroadcasting(false);
 				
-				send_file(m_link->m_ch, 1, 0, f.toLocal8Bit().constData());
+				send_file(m_link->m_ch, (m_files.count() == 0),
+					  f.toLocal8Bit().constData());
 				
 				m_link->m_calc->setBroadcasting(broadcast);
 				
@@ -495,14 +496,14 @@ static CableHandle* internal_link_handle_new(Calc *calc)
 {
 	CableHandle* cbl = ticables_handle_new(CABLE_ILP, PORT_0);
 	
-	if ( cbl )
-	{
+	if (cbl) {
 		cbl->priv = calc;
 		cbl->cable->reset = ilp_reset;
 		cbl->cable->send = ilp_send;
 		cbl->cable->recv = ilp_recv;
 		cbl->cable->check = ilp_check;
-	} else {
+	}
+	else {
 		qDebug("unable to create internal link handle");
 	}
 	
@@ -513,21 +514,14 @@ static CableHandle* external_link_handle_new()
 {
 	CableHandle* cbl = ticables_handle_new(CABLE_TIE, PORT_0);
 	
-	if ( cbl )
-	{
+	if (cbl) {
 		ticables_options_set_timeout(cbl, DFLT_TIMEOUT);
 		ticables_options_set_delay(cbl, DFLT_DELAY);
-		
-		cbl->priv = 0;
-// 		cbl->cable->reset = ilp_reset;
-// 		cbl->cable->send = ilp_send;
-// 		cbl->cable->recv = ilp_recv;
-// 		cbl->cable->check = ilp_check;
-	} else {
+		ticables_cable_open(cbl);
+	}
+	else {
 		qDebug("unable to create external link handle");
 	}
-	
-	ticables_cable_open(cbl);
 	
 	return cbl;
 }
@@ -549,42 +543,96 @@ static int print_tilibs_error(int errcode)
 	return errcode;
 }
 
-static void send_file(CalcHandle* ch, int first, int last, const char* filename)
+static int send_file(CalcHandle* ch, int last, const char* filename)
 {
-	Q_UNUSED(first)
-	
-	CalcScreenCoord sc;
-	uint8_t *bitmap = NULL;
-	
-	switch ( tifiles_file_get_class(filename) )
-	{
+	CalcMode mode;
+	FileContent* filec;
+	BackupContent* backupc;
+	FlashContent* flashc;
+	TigContent* tigc;
+	int err;
+
+	switch (tifiles_file_get_class(filename)) {
 	case TIFILE_SINGLE:
 	case TIFILE_GROUP:
 	case TIFILE_REGULAR:
-		print_tilibs_error(ticalcs_calc_send_var2(ch, static_cast<CalcMode>(last ? MODE_SEND_LAST_VAR : 0), filename));
+		filec = tifiles_content_create_regular(ch->model);
+		err = tifiles_file_read_regular(filename, filec);
+
+		if (err) {
+			print_tilibs_error(err);
+			tifiles_content_delete_regular(filec);
+			return 1;
+		}
+
+		if (last)
+			mode = MODE_SEND_LAST_VAR;
+		else
+			mode = MODE_NORMAL;
+
+		err = ticalcs_calc_send_var(ch, mode, filec);
+		tifiles_content_delete_regular(filec);
+		break;
+
+	case TIFILE_BACKUP:
+		backupc = tifiles_content_create_backup(ch->model);
+		err = tifiles_file_read_backup(filename, backupc);
+
+		if (err) {
+			print_tilibs_error(err);
+			tifiles_content_delete_backup(backupc);
+			return 1;
+		}
+
+		err = ticalcs_calc_send_backup(ch, backupc);
+		tifiles_content_delete_backup(backupc);
 		break;
 
 	case TIFILE_FLASH:
 	case TIFILE_OS:
 	case TIFILE_APP:
+		flashc = tifiles_content_create_flash(ch->model);
+		err = tifiles_file_read_flash(filename, flashc);
+
+		if (err) {
+			print_tilibs_error(err);
+			tifiles_content_delete_flash(flashc);
+			return 1;
+		}
+
 		if (tifiles_file_is_os(filename))
-			print_tilibs_error(ticalcs_calc_send_os2(ch, filename));
+			err = print_tilibs_error(ticalcs_calc_send_os(ch, flashc));
 		else if (tifiles_file_is_app(filename))
-			print_tilibs_error(ticalcs_calc_send_app2(ch, filename));
+			err = print_tilibs_error(ticalcs_calc_send_app(ch, flashc));
+		else
+			err = print_tilibs_error(ticalcs_calc_send_cert(ch, flashc));
+
+		tifiles_content_delete_flash(flashc);
 		break;
 
 	case TIFILE_TIGROUP:
-		print_tilibs_error(ticalcs_calc_send_tigroup2(ch, filename, TIG_ALL));
+		tigc = tifiles_content_create_tigroup(ch->model, 0);
+		err = tifiles_file_read_tigroup(filename, tigc);
+
+		if (err) {
+			print_tilibs_error(err);
+			tifiles_content_delete_tigroup(tigc);
+			return 1;
+		}
+
+		err = ticalcs_calc_send_tigroup(ch, tigc, TIG_ALL);
 		break;
 
 	default:
-		if (1)
-			print_tilibs_error(ticalcs_calc_send_key(ch, 0xA0));
-		else {
-			print_tilibs_error(ticalcs_calc_recv_screen(ch, &sc, &bitmap));
-			free(bitmap);
-		}
-		break;
+		return 1;
+	}
+
+	if (err) {
+		print_tilibs_error(err);
+		return -1;
+	}
+	else {
+		return 0;
 	}
 }
 #endif
