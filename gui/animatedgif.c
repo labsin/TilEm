feae679f307c93ca8f7600bd4d08ab8c32c40f34
contra-sh <contra-sh@05cf5367-e1a8-405a-8a5c-7f956f11530a>
2011-02-17 19:51:02 +0000

    Fix a serious mistake in the creation of animated gif. Now, web browser can read the gif.Palette is defined correctly and contains 256 colors here.Gif color is black and white not black and green (only use 2 colors of the palette currently). Lot of improvement is possible (choose the delay, choose the palette, etc...).
    git-svn-id: svn://svn.code.sf.net/p/tilem/code/trunk@201 05cf5367-e1a8-405a-8a5c-7f956f11530a

--- gui/animatedgif.c
+++ gui/animatedgif.c
@@ -70,17 +70,30 @@ void screenshot_anim_create(GLOBAL_SKIN_INFOS* gsi) {
     
 void screenshot_anim_create_nostatic(GLOBAL_SKIN_INFOS* gsi) {
 	
-	char gif_header[13] = {'G', 'I', 'F', '8', '9', 'a', 96, 0, 64, 0, 0x80, 0x0f, 0};
-    	char gif_infos[31] = {
+	/* The 11th byte is a set of flags  : 
+	bit 0:    Global Color Table Flag (GCTF)
+        bit 1..3: Color Resolution
+        bit 4:    Sort Flag to Global Color Table
+        bit 5..7: Size of Global Color Table: 2^(1+n)
+	It means "use the GCT wich is given after (from the size bit 5..7) and a resolution bit 1..3 
+	The Background color is an index in the Global Color Table
+	*/
+	
+	/* Magic number (GIF89a), width pixel 2 bytes, height pixel 2 bytes, GCT follows 256 values * 3 bytes (RGB) , bg color, default aspect ration */ 	
+	char gif_header[13] = {'G', 'I', 'F', '8', '9', 'a', 96, 0, 64, 0, 0xf7, 0x00, 0};
+    	/* Introduce the block 3bytes, netscape (type of gif : animation), a new block comment */
+	char gif_infos[31] = {
         0x21, 0xff, 0x0b, 'N', 'E', 'T', 'S', 'C', 'A', 'P', 'E', '2', '.', '0', 3, 1, 0, 0, 0,
 	0x21, 0xfe, 8, 'T', 'i', 'l', 'E', 'm', '2', 0, 0, 0
 	};
 	
 	int width, height;
 	guchar* lcddata;
+	int k;
 	int x, y;
 	width = gsi->emu->calc->hw.lcdwidth;
 	height = gsi->emu->calc->hw.lcdheight;
+	
 	/* Alloc mmem */
 	lcddata = g_new(guchar, (width / 8) * height);
 		
@@ -94,39 +107,41 @@ void screenshot_anim_create_nostatic(GLOBAL_SKIN_INFOS* gsi) {
 	printf("GIF ENCODER\n");
 	FILE* fp;
 	fp = fopen("gifencod.gif", "w");
-    	/* Magic number for Gif file format */
-    	//char magic_number[] = {'G', 'I', 'F', '8', '9', 'a'};
-    	/* Size of canvas width on 2 bytes, heigth on 2 bytes, GCT , bg color i (place dans la palette ici c'est blanc) , aspect pixel ratio */
-    	//char canvas[] = { 96, 0, 64, 0, 0x80, 0x0f, 0x00};
-   
-    	char palette[] = { 0xff, 0xff, 0xff, 0x00, 0x00, 0x00};
+  	
+	/* TODO : convert this padding stuff to a real palette */ 
+    	char palette_start[] = { 0x00, 0x00, 0x00};
+	char padding[] = { 0xff, 0xff, 0xff};
+	char palette_end[] = { 0xff, 0xff, 0xff};
 
     
     	/* Extension block introduced by 0x21 ('!'), and an img introduced by 0x2c (',') followed by coordinate corner(0,0), canvas 4 bytes, no local color table */
 	static char gif_img[18] = {0x21, 0xf9, 4, 5, 11, 0, 0x0f, 0, 0x2c, 0, 0, 0, 0, 96, 0, 64, 0, 0};
-	//static unsigned char example[] = { 0x01, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01};
     
     	char end[1] = { 0xC3};
         
 	fwrite(gif_header, 13, 1, fp);
+    	fwrite(palette_start, 3, 1, fp);
+	for(k = 0; k < 254; k++) {
+		fwrite(padding, 3, 1, fp);
+	}
+    	fwrite(palette_end, 3, 1, fp);
     	fwrite(gif_infos, 31, 1, fp);
-    	fwrite(palette, 6, 1, fp);
     	fwrite(gif_img, 18, 1, fp);
     	
-	//fwrite(example, 19, 1, fp);
 	long i= 0;
 	
 	unsigned char q[(96*64)];
 
 
+	/* Reduce screen buffer to one byte per pixel */
 	for (y = 0; y < height; y++) {
 		for (x = 0; x < width; x++) {
 			if (lcddata[(y * width + x) / 8] & (0x80 >> (x % 8))) {
 				//printf("i = %d", i);
-				q[i] = 0x01;
+				q[i] = 0x00;
 				i++;
 			} else {
-				q[i] = 0x00;
+				q[i] = 0x02;
 				i++;
 			}
 		}
@@ -134,13 +149,13 @@ void screenshot_anim_create_nostatic(GLOBAL_SKIN_INFOS* gsi) {
 	
 	
 
-	GifEncode(fp, q , 1, (96*64));
-	fwrite(end, 1, 1,fp);
+	GifEncode(fp, q , 1, (width*height));
+	fwrite(end, 1, 1,fp);	/* Write end of the frame */
 	fclose(fp);
 }
     
 
-
+/* Add a frame to an existing animated gif */
 void screenshot_anim_addframe(GLOBAL_SKIN_INFOS* gsi) {
 	
 
@@ -159,38 +174,23 @@ void screenshot_anim_addframe(GLOBAL_SKIN_INFOS* gsi) {
 	FILE* fp;
 	fp = fopen("gifencod.gif", "a");
     	
-	
-    	/* Size of canvas width on 2 bytes, heigth on 2 bytes, GCT , bg color i (place dans la palette ici c'est blanc) , aspect pixel ratio */
-    	//char canvas[] = { 96, 0, 64, 0, 0x80, 0x0f, 0x00};
-   
-    	/* GCT, multiple de 2 */ 
-    	//char palette[] = { 0xff, 0xff, 0xff, 0x00, 0x00, 0x00};
-
-    
     	/* Extension block introduced by 0x21 ('!'), and an img introduced by 0x2c (',') followed by coordinate corner(0,0), canvas 4 bytes, no local color table */
-	static char gif_img[18] = {0x21, 0xf9, 4, 5, 11, 0, 0x0f, 0, 0x2c, 0, 0, 0, 0, 96, 0, 64, 0, 0};
-	//static unsigned char example[] = { 0x01, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01};
-    
+	static char gif_img[18] = {0x21, 0xf9, 4, 8, 11, 0, 0x0f, 0, 0x2c, 0, 0, 0, 0, 96, 0, 64, 0, 0};
     	char end[1] = { 0xC3};
         
-    	//fwrite(canvas, 7, 1, fp);
-    	//fwrite(palette, 6, 1, fp);
     	fwrite(gif_img, 18, 1, fp);
-    	
-	//fwrite(example, 19, 1, fp);
 	long i= 0;
 	
-	unsigned char q[(96*64)];
+	unsigned char q[(width * height)];
 
-
+	/* Reduce screen buffer to one byte per pixel */
 	for (y = 0; y < height; y++) {
 		for (x = 0; x < width; x++) {
 			if (lcddata[(y * width + x) / 8] & (0x80 >> (x % 8))) {
-				//printf("i = %d", i);
-				q[i] = 0x01;
+				q[i] = 0x00;
 				i++;
 			} else {
-				q[i] = 0x00;
+				q[i] = 0x02;
 				i++;
 			}
 		}
@@ -198,7 +198,7 @@ void screenshot_anim_addframe(GLOBAL_SKIN_INFOS* gsi) {
 	
 	
 
-	GifEncode(fp, q , 1, (96*64));
+	GifEncode(fp, q , 1, (width*height));
 	fwrite(end, 1, 1,fp);
 	fclose(fp);
 }
