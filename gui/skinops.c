8a59c700126e1542dc43ee32204ab8b0ddeb90af
floppusmaximus <floppusmaximus@05cf5367-e1a8-405a-8a5c-7f956f11530a>
2011-03-02 01:49:31 +0000

    clean up formatting/indentation
    git-svn-id: svn://svn.code.sf.net/p/tilem/code/trunk@208 05cf5367-e1a8-405a-8a5c-7f956f11530a

--- gui/skinops.c
+++ gui/skinops.c
@@ -1,6 +1,3 @@
-/* Hey EMACS -*- linux-c -*- */
-/* $Id$ */
-
 /*
  *   skinedit - a skin editor for the TiEmu emulator
  *   Copyright (C) 2002 Julien BLACHE <jb@tilp.info>
@@ -20,14 +17,11 @@
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-
 /*
 contra-sh :
    This file is a perfect copy of the tiemu skinops.c file ...
    Thank's to rom's and JB for this wonderful work.
 */
-
-
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -44,14 +38,8 @@ contra-sh :
 #include <debuginfo.h>
 #include "skinops.h"
 
-
 #include <gtk/gtk.h>
 #include <glib/gstdio.h>
-
-
-
-
-
 
 /*
 	Determine skin type
@@ -62,11 +50,10 @@ int skin_get_type(SKIN_INFOS *si, const char *filename)
 	char str[17];
 
 	fp = fopen(filename, "rb");
-  	if (fp == NULL)
-    {
-      	fprintf(stderr, "Unable to open this file: <%s>\n", filename);
-      	return -1;
-    }
+	if (fp == NULL) {
+		fprintf(stderr, "Unable to open this file: <%s>\n", filename);
+		return -1;
+	}
 
 	memset(str, 0, sizeof(str));
 	fread(str, 16, 1, fp);
@@ -79,121 +66,113 @@ int skin_get_type(SKIN_INFOS *si, const char *filename)
 		si->type = SKIN_TYPE_OLD_VTI;
 	else if(!strncmp(str, "VTIv2.5 ", 8))
 		si->type = SKIN_TYPE_VTI;
-	else
-  	{
-  		fprintf(stderr, "Bad skin format\n");
-      	return -1;
-  	}
+	else {
+		fprintf(stderr, "Bad skin format\n");
+		return -1;
+	}
 
 	return 0;
 }
 
-
 /*
-	Read TilEm skin informations (header)
+  Read TilEm skin informations (header)
 */
 int skin_read_header(SKIN_INFOS *si, const char *filename)
 {
 	FILE *fp;
-  	int i;
-  	uint32_t endian;
-  	uint32_t jpeg_offset;
-  	uint32_t length;
+	int i;
+	uint32_t endian;
+	uint32_t jpeg_offset;
+	uint32_t length;
 	char str[17];
 
 	fp = fopen(filename, "rb");
-  	if (fp == NULL)
-    {
-      	fprintf(stderr, "Unable to open this file: <%s>\n", filename);
-      	return -1;
-    }
- 
+	if (fp == NULL) {
+		fprintf(stderr, "Unable to open this file: <%s>\n", filename);
+		return -1;
+	}
+
 	/* signature & offsets */
 	fread(str, 16, 1, fp);
-  	if ((strncmp(str, "TilEm v2.00", 16))&&(strncmp(str, "TiEmu v2.00", 16)))
-  	{
-  		fprintf(stderr, "Bad TiEmu skin format\n");
-      	return -1;
-  	}
-  	fread(&endian, 4, 1, fp);
-  	fread(&jpeg_offset, 4, 1, fp);
+	if ((strncmp(str, "TilEm v2.00", 16))
+	    && (strncmp(str, "TiEmu v2.00", 16))) {
+		fprintf(stderr, "Bad TiEmu skin format\n");
+		return -1;
+	}
+	fread(&endian, 4, 1, fp);
+	fread(&jpeg_offset, 4, 1, fp);
 
 	if (endian != ENDIANNESS_FLAG)
 		jpeg_offset = GUINT32_SWAP_LE_BE(jpeg_offset);
 
 	/* Skin name */
-  	fread(&length, 4, 1, fp);
+	fread(&length, 4, 1, fp);
 	if (endian != ENDIANNESS_FLAG)
 		length = GUINT32_SWAP_LE_BE(length);
 
-  	if (length > 0)
-    {
-      	si->name = (char *)malloc(length + 1);
-	    if (si->name == NULL)
+	if (length > 0) {
+		si->name = (char *)malloc(length + 1);
+		if (si->name == NULL)
 			return -1;
 
-	    memset(si->name, 0, length + 1);
-	    fread(si->name, length, 1, fp);
-    }
-	
+		memset(si->name, 0, length + 1);
+		fread(si->name, length, 1, fp);
+	}
+
 	/* Skin author */
-  	fread(&length, 4, 1, fp);
+	fread(&length, 4, 1, fp);
 	if (endian != ENDIANNESS_FLAG)
 		length = GUINT32_SWAP_LE_BE(length);
 
-  	if (length > 0)
-    {
-      	si->author = (char *)malloc(length + 1);
-      	if (si->author == NULL)
+	if (length > 0) {
+		si->author = (char *)malloc(length + 1);
+		if (si->author == NULL)
 			return -1;
 
-      	memset(si->author, 0, length + 1);
-      	fread(si->author, length, 1, fp);
-    }
+		memset(si->author, 0, length + 1);
+		fread(si->author, length, 1, fp);
+	}
 
 	/* LCD colors */
-  	fread(&si->colortype, 4, 1, fp);
-  	fread(&si->lcd_white, 4, 1, fp);
-  	fread(&si->lcd_black, 4, 1, fp);
+	fread(&si->colortype, 4, 1, fp);
+	fread(&si->lcd_white, 4, 1, fp);
+	fread(&si->lcd_black, 4, 1, fp);
 
-   	/* Calc type */
-  	fread(si->calc, 8, 1, fp);
+	/* Calc type */
+	fread(si->calc, 8, 1, fp);
 
-  	/* LCD position */
-  	fread(&si->lcd_pos.left, 4, 1, fp);
-  	fread(&si->lcd_pos.top, 4, 1, fp);
-  	fread(&si->lcd_pos.right, 4, 1, fp);
-  	fread(&si->lcd_pos.bottom, 4, 1, fp);
+	/* LCD position */
+	fread(&si->lcd_pos.left, 4, 1, fp);
+	fread(&si->lcd_pos.top, 4, 1, fp);
+	fread(&si->lcd_pos.right, 4, 1, fp);
+	fread(&si->lcd_pos.bottom, 4, 1, fp);
 
 	/* Number of RECT struct to read */
-  	fread(&length, 4, 1, fp);
+	fread(&length, 4, 1, fp);
 	if (endian != ENDIANNESS_FLAG)
 		length = GUINT32_SWAP_LE_BE(length);
 
-  	if (length > SKIN_KEYS)
-    		return -1;
+	if (length > SKIN_KEYS)
+		return -1;
 
-  	for (i = 0; i < (int)length; i++)
-    {
-      	fread(&si->keys_pos[i].left, 4, 1, fp);
-      	fread(&si->keys_pos[i].top, 4, 1, fp);
-      	fread(&si->keys_pos[i].right, 4, 1, fp);
-      	fread(&si->keys_pos[i].bottom, 4, 1, fp);
-    }
+	for (i = 0; i < (int)length; i++) {
+		fread(&si->keys_pos[i].left, 4, 1, fp);
+		fread(&si->keys_pos[i].top, 4, 1, fp);
+		fread(&si->keys_pos[i].right, 4, 1, fp);
+		fread(&si->keys_pos[i].bottom, 4, 1, fp);
+	}
 
-	if (endian != ENDIANNESS_FLAG)
-	{
+	if (endian != ENDIANNESS_FLAG) {
 		si->colortype = GUINT32_SWAP_LE_BE(si->colortype);
 		si->lcd_white = GUINT32_SWAP_LE_BE(si->lcd_white);
 		si->lcd_black = GUINT32_SWAP_LE_BE(si->lcd_black);
-      
+
 		si->lcd_pos.top = GUINT32_SWAP_LE_BE(si->lcd_pos.top);
 		si->lcd_pos.left = GUINT32_SWAP_LE_BE(si->lcd_pos.left);
 		si->lcd_pos.bottom = GUINT32_SWAP_LE_BE(si->lcd_pos.bottom);
 		si->lcd_pos.right = GUINT32_SWAP_LE_BE(si->lcd_pos.right);
 
-		for (i = 0; i < (int)length; i++)
-		{
+		for (i = 0; i < (int)length; i++) {
 			si->keys_pos[i].top = GUINT32_SWAP_LE_BE(si->keys_pos[i].top);
 			si->keys_pos[i].bottom = GUINT32_SWAP_LE_BE(si->keys_pos[i].bottom);
 			si->keys_pos[i].left = GUINT32_SWAP_LE_BE(si->keys_pos[i].left);
@@ -202,22 +181,21 @@ int skin_read_header(SKIN_INFOS *si, const char *filename)
 	}
 
 	si->jpeg_offset = ftell(fp);
-    	
-    fclose(fp);
-    return 0;
+
+	fclose(fp);
+	return 0;
 }
 
-
 /*
-	Read skin image (pure jpeg data)
+  Read skin image (pure jpeg data)
 */
 int skin_read_image(SKIN_INFOS *si, const char *filename)
 {
 	DSKIN_L2_A0("* -> fct : skin_read_image                             *\n");
-    FILE *fp = NULL;	
+	FILE *fp = NULL;
 	int i;
 	int sw, sh;
-    int lw, lh;
+	int lw, lh;
 	float rw, rh, r;
 	double s;
 	int lcd_w, lcd_h;
@@ -230,65 +208,59 @@ int skin_read_image(SKIN_INFOS *si, const char *filename)
 	struct stat st;
 
 	// set lcd size
-	if(!strcmp(si->calc, SKIN_TI89) || !strcmp(si->calc, SKIN_TI89T))
-	{
+	if(!strcmp(si->calc, SKIN_TI89) || !strcmp(si->calc, SKIN_TI89T)) {
 		lcd_w = 160;
 		lcd_h = 100;
-	} 
-	else
-	{
+	}
+	else {
 		lcd_w = 240;
 		lcd_h = 128;
 	}
-  
-    	fp = fopen(filename, "rb");
-  	if (fp == NULL)
-    	{
-    		fprintf(stderr, "Unable to open this file: <%s>\n", filename);
-    		return -1;
-    	}
-    	
-    // Extract image from skin
+
+	fp = fopen(filename, "rb");
+	if (fp == NULL) {
+		fprintf(stderr, "Unable to open this file: <%s>\n", filename);
+		return -1;
+	}
+
+	// Extract image from skin
 	fseek(fp, si->jpeg_offset, SEEK_SET);
 	fstat(fileno(fp), &st);
 	count = st.st_size - si->jpeg_offset;
 
 	buf = g_malloc(count * sizeof(guchar));
 	count = fread(buf, sizeof(guchar), count, fp);
-	
+
 	// Feed the pixbuf loader with our jpeg data
-	loader = gdk_pixbuf_loader_new();	
+	loader = gdk_pixbuf_loader_new();
 	result = gdk_pixbuf_loader_write(loader, buf, count, &error);
-	if(result == FALSE)
-	{
+	if(result == FALSE) {
 		fprintf(stderr, "Failed to load pixbuf file: %s\n", filename);
 		g_error_free(error);
 
 		return -1;
 	}
-    
+
 	result = gdk_pixbuf_loader_close(loader, &error);
-	if(result == FALSE)
-	{
+	if(result == FALSE) {
 		fprintf(stderr, "Failed to close pixbuf file: %s\n", filename);
 		g_error_free(error);
 
 		return -1;
 	}
-	
-    // and get the pixbuf
+
+	// and get the pixbuf
 	si->raw = gdk_pixbuf_loader_get_pixbuf(loader);
-	if(si->raw == NULL)
-	{
+	if(si->raw == NULL) {
 		fprintf(stderr, "Failed to load pixbuf file: %s\n", filename);
 		g_error_free(error);
 
 		return -1;
-    }
+	}
 
 	// Rescale image to a fixed LCD size but keep original image (avoid resolution lost)
-    sw = gdk_pixbuf_get_width(si->raw);
-    sh = gdk_pixbuf_get_height(si->raw);
+	sw = gdk_pixbuf_get_width(si->raw);
+	sh = gdk_pixbuf_get_height(si->raw);
 
 	lw = si->lcd_pos.right - si->lcd_pos.left;
 	lh = si->lcd_pos.bottom - si->lcd_pos.top;
@@ -302,14 +274,14 @@ int skin_read_image(SKIN_INFOS *si, const char *filename)
 	DSKIN_L0_A2("*  image :<%i x %i>                                  *\n", sw, sh);
 	DSKIN_L0_A2("*  lcd : <%i x %i>                                   *\n", lw, lh);
 	DSKIN_L0_A3("*  ratios : <%2.2f x %2.2f> => %2.1f                       *\n", rw, rh, s);
-    
-	//if(si->image)
-		//g_object_unref(si->image);
-    si->image = gdk_pixbuf_scale_simple(si->raw, (int)(sw/s), (int)(sh/s), GDK_INTERP_NEAREST);
 
-    // Get new skin size
-    si->width = gdk_pixbuf_get_width(si->image);
-    si->height = gdk_pixbuf_get_height(si->image);
+	//if(si->image)
+	//g_object_unref(si->image);
+	si->image = gdk_pixbuf_scale_simple(si->raw, (int)(sw/s), (int)(sh/s), GDK_INTERP_NEAREST);
+
+	// Get new skin size
+	si->width = gdk_pixbuf_get_width(si->image);
+	si->height = gdk_pixbuf_get_height(si->image);
 
 	// Rescale all coords
 	si->lcd_pos.left = (long)(si->lcd_pos.left / s);
@@ -317,52 +289,50 @@ int skin_read_image(SKIN_INFOS *si, const char *filename)
 	si->lcd_pos.top = (long)(si->lcd_pos.top / s);
 	si->lcd_pos.bottom = (long)(si->lcd_pos.bottom / s);
 
-	for (i = 0; i < SKIN_KEYS; i++) 
-	{
-      	si->keys_pos[i].left = (long)(si->keys_pos[i].left / s);
-      	si->keys_pos[i].top = (long)(si->keys_pos[i].top / s);
-      	si->keys_pos[i].right = (long)(long)(si->keys_pos[i].right / s);
-      	si->keys_pos[i].bottom = (long)(long)(si->keys_pos[i].bottom / s);
-    }
+	for (i = 0; i < SKIN_KEYS; i++) {
+		si->keys_pos[i].left = (long)(si->keys_pos[i].left / s);
+		si->keys_pos[i].top = (long)(si->keys_pos[i].top / s);
+		si->keys_pos[i].right = (long)(long)(si->keys_pos[i].right / s);
+		si->keys_pos[i].bottom = (long)(long)(si->keys_pos[i].bottom / s);
+	}
 
-   	return 0;
+	return 0;
 }
 
 /* Load a skin (TilEm v2.00 only) */
 int skin_load(SKIN_INFOS *si, const char *filename)
 {
-  	int ret = 0;
+	int ret = 0;
 	DSKIN_L0_A0("**************** fct : skin_load ***********************\n");
-  	ret = skin_read_header(si, filename);
+	ret = skin_read_header(si, filename);
 	if(ret)
 		return ret;
 
-  	ret = skin_read_image(si, filename);
+	ret = skin_read_image(si, filename);
 	if(ret)
 		return ret;
 
 	if(!ret)
-   		printf("*  loading skin:  (%d x %d)                          *\n", si->width, si->height);
-	
+		printf("*  loading skin:  (%d x %d)                          *\n", si->width, si->height);
+
 	DGLOBAL_L0_A0("********************************************************\n");
-  
-  	return ret;
+
+	return ret;
 }
 
 /* Unload skin by freeing allocated memory */
 int skin_unload(SKIN_INFOS *si)
 {
-    if(si->image != NULL)
-    {
-        g_object_unref(si->image);
-        si->image = NULL;
-    }
+	if(si->image != NULL) {
+		g_object_unref(si->image);
+		si->image = NULL;
+	}
 
-  	free(si->name);
-  	free(si->author);
+	free(si->name);
+	free(si->author);
 
-  	memset(si, 0, sizeof(SKIN_INFOS));
-  
-  	return 0;
+	memset(si, 0, sizeof(SKIN_INFOS));
+
+	return 0;
 }
 
