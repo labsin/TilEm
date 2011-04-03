6fde6a5cd1eaa617736712f5d7eb35fcfe7631fe
floppusmaximus <floppusmaximus@05cf5367-e1a8-405a-8a5c-7f956f11530a>
2011-04-03 22:56:14 +0000

    New key binding syntax and a mostly rewritten parser.
    - Use standard X key names on host side
    - Use model-defined names on calculator side
    - Multiple calculator keys separated by ','
    - Host keys may be preceded by a list of modifiers, separated by '+'
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/tilem/code/trunk@256 05cf5367-e1a8-405a-8a5c-7f956f11530a

--- gui/keybindings.c
+++ gui/keybindings.c
@@ -18,125 +18,178 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
 
-#include "keybindings.h"
+#include <stdio.h>
+#include <string.h>
+#include <gtk/gtk.h>
 
+#include "gui.h"
+#include "msgbox.h"
 
-void tilem_keybindings_init(GLOBAL_SKIN_INFOS *gsi, const char* model) {
+static int calc_key_from_name(const TilemCalc *calc, const char *name)
+{
+	int i;
 
-	//tilem_ksc_get_scancode("GDK_A", "ti83") ;
-	
-	/* Test if there's a group for the current model */	
-	if(!tilem_test_group_exist_from_config_file("keybindings.ini", (char*)model))
-		return ;
+	for (i = 0; i < 64; i++)
+		if (calc->hw.keynames[i]
+		    && !strcmp(calc->hw.keynames[i], name))
+			return i + 1;
 
-	
-	gchar ** keys = tilem_ksc_get_all_keys(model);
+	return 0;
+}
 
-	int i = 0;
-	int n = 0;
+static gboolean parse_binding(TilemKeyBinding *kb,
+                              const char *pckeys, const char *tikeys,
+                              const TilemCalc *calc)
+{
+	const char *p;
+	char *s;
+	int n, k;
+
+	kb->modifiers = 0;
+	kb->keysym = 0;
+	kb->nscancodes = 0;
+	kb->scancodes = NULL;
+
+	/* Parse modifiers */
+
+	while ((p = strchr(pckeys, '+'))) {
+		s = g_strndup(pckeys, p - pckeys);
+		g_strstrip(s);
+		if (!g_ascii_strcasecmp(s, "ctrl")
+		    || !g_ascii_strcasecmp(s, "control"))
+			kb->modifiers |= GDK_CONTROL_MASK;
+		else if (!g_ascii_strcasecmp(s, "shift"))
+			kb->modifiers |= GDK_SHIFT_MASK;
+		else if (!g_ascii_strcasecmp(s, "alt")
+		         || !g_ascii_strcasecmp(s, "mod1"))
+			kb->modifiers |= GDK_MOD1_MASK;
+		else if (!g_ascii_strcasecmp(s, "mod2"))
+			kb->modifiers |= GDK_MOD2_MASK;
+		else if (!g_ascii_strcasecmp(s, "mod3"))
+			kb->modifiers |= GDK_MOD3_MASK;
+		else if (!g_ascii_strcasecmp(s, "mod4"))
+			kb->modifiers |= GDK_MOD4_MASK;
+		else if (!g_ascii_strcasecmp(s, "mod5"))
+			kb->modifiers |= GDK_MOD5_MASK;
+		else {
+			g_free(s);
+			return FALSE;
+		}
+		g_free(s);
+		pckeys = p + 1;
+	}
+
+	/* Parse keysym */
+
+	s = g_strstrip(g_strdup(pckeys));
+	kb->keysym = gdk_keyval_from_name(s);
+	g_free(s);
+	if (!kb->keysym)
+		return FALSE;
+
+	/* Parse calculator keys */
+
+	/* FIXME: allow combinations of simultaneous keys (separated
+	   by '+'); current TilemKeyBinding struct doesn't provide for
+	   this */
+
+	n = 0;
+	do {
+		if ((p = strchr(tikeys, ',')))
+			s = g_strndup(tikeys, p - tikeys);
+		else
+			s = g_strdup(tikeys);
+		g_strstrip(s);
+
+		k = calc_key_from_name(calc, s);
+		g_free(s);
+
+		if (!k) {
+			g_free(kb->scancodes);
+			kb->scancodes = NULL;
+			return FALSE;
+		}
+
+		kb->nscancodes++;
+		if (kb->nscancodes >= n) {
+			n = kb->nscancodes * 2;
+			kb->scancodes = g_renew(byte, kb->scancodes, n);
+		}
+		kb->scancodes[kb->nscancodes - 1] = k;
+
+		tikeys = (p ? p + 1 : NULL);
+	} while (tikeys);
+
+	return TRUE;
+}
+
+static void parse_binding_group(GLOBAL_SKIN_INFOS *gsi, GKeyFile *gkf,
+                                const char *group)
+{
+	gchar **keys;
+	char *k, *v;
+	int i, n;
+
+	keys = g_key_file_get_keys(gkf, group, NULL, NULL);
+	if (!keys) {
+		printf("no bindings for %s\n", group);
+		return;
+	}
+
+	for (i = 0; keys[i]; i++)
+		;
+
+	n = gsi->nkeybindings;
+	gsi->keybindings = g_renew(TilemKeyBinding, gsi->keybindings, n + i);
+
 	for(i = 0; keys[i]; i++) {
-		n++;
-		printf("keys %d : %s\n", i, keys[i]);
+		k = keys[i];
+		v = g_key_file_get_value(gkf, group, k, NULL);
+		if (!v)
+			continue;
+
+		if (parse_binding(&gsi->keybindings[n], k, v, gsi->emu->calc))
+			n++;
+		else
+			g_printerr("syntax error in key bindings: '%s=%s'\n",
+			           k, v);
+		g_free(v);
 	}
 
 	gsi->nkeybindings = n;
-	gsi->keybindings = g_new(TilemKeyBinding, n);
 
-	for(i = 0; keys[i]; i++) {
-		gsi->keybindings[i].keysym = tilem_get_gdkkeysyms(gks, keys[i]);
-		char* scancodestr = tilem_ksc_get_scancode(keys[i], model);
-		printf("scancodestr : %s \n", scancodestr);
-	
-		if(scancodestr) {
-			char* p = strrchr(scancodestr, ':');
-			if(p) {
-				gsi->keybindings[i].modifiers = 0;
-				gsi->keybindings[i].nscancodes = 2;
-				gsi->keybindings[i].scancodes = g_new(byte, 2);
-				gsi->keybindings[i].scancodes[1] = tilem_get_tilemkeysyms(tks, ++p);
-				--p;
-				p[0] = '\0';
-				gsi->keybindings[i].scancodes[0] = tilem_get_tilemkeysyms(tks, scancodestr);
-				printf("keysym= %d\tscancode[0]= %d\tscancode[1]= %d\n", gsi->keybindings[i].keysym, gsi->keybindings[i].scancodes[0],gsi->keybindings[i].scancodes[1]);
-			} else {	
-				gsi->keybindings[i].modifiers = 0;
-				gsi->keybindings[i].nscancodes = 1;
-				gsi->keybindings[i].scancodes = g_new(byte, 1);
-				gsi->keybindings[i].scancodes[0] = tilem_get_tilemkeysyms(tks, tilem_ksc_get_scancode(keys[i], model));
-				printf("keysym= %d\tscancode[0]= %d\n", gsi->keybindings[i].keysym, gsi->keybindings[i].scancodes[0]);
-			}
-			p = NULL;
-		}
-		free(scancodestr);
-	}
+	g_strfreev(keys);
 }
 
-/* Translating a string name (by example "GDK_A") to a unsigned int value (code for GDK_A) */
-unsigned int tilem_get_gdkkeysyms(GdkKeySyms *gks, char* string) {
-	int i= 0;
+void tilem_keybindings_init(GLOBAL_SKIN_INFOS *gsi, const char *model)
+{
+	const char *kfname = "keybindings.ini"; /* FIXME */
+	GKeyFile *gkf;
+	GError *err = NULL;
 
-	for(i= 0; gks[i].keysymstr; i++) {
-		if(strcmp(string, gks[i].keysymstr) == 0) 
-			return gks[i].keysym;
-	}
-	
-	return 0;
+	g_return_if_fail(gsi != NULL);
+	g_return_if_fail(gsi->emu != NULL);
+	g_return_if_fail(gsi->emu->calc != NULL);
 
-} 
-
-/* Translating a string name (by example "TILEM_KEY_A") to a unsigned int value (code for TILEM_KEY_A) */
-unsigned int tilem_get_tilemkeysyms(TilemKeySyms *tks, char* string) {
-	int i= 0;
-	
-	for(i= 0; tks[i].scancodestr; i++) {
-		if(strcmp(string, tks[i].scancodestr) == 0) {
-			//printf("%s : code %d\n", string, tks[i].scancode);
-			return tks[i].scancode;
-		}
-	}
-	return 0;
-}
-
-/* Search the scancode into the keysyms config file */
-char* tilem_ksc_get_scancode(char* gdkkeysymstr, const char* model) {
-
-	GKeyFile * gkf;
 	gkf = g_key_file_new();
-	
-	if (! g_key_file_load_from_file(gkf, KEYBINDINGS_FILE, G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS, NULL)) {
-		fprintf(stderr, "Could not read config file '%s'\n", KEYBINDINGS_FILE);
-		exit(EXIT_FAILURE);
+	if (!g_key_file_load_from_file(gkf, kfname, 0, &err)) {
+		messagebox02(NULL, GTK_MESSAGE_ERROR,
+		             "Unable to load key bindings",
+		             "An error occurred while reading %s: %s",
+		             kfname, err->message);
+		g_error_free(err);
+		return;
 	}
 
-	char* scancodestr = g_key_file_get_string(gkf, model, gdkkeysymstr, NULL);
-	//printf("scancodestr : %s\n", scancodestr);
-	
+	g_free(gsi->keybindings);
+	gsi->keybindings = NULL;
+	gsi->nkeybindings = 0;
+
+	parse_binding_group(gsi, gkf, model);
+
 	g_key_file_free(gkf);
-	return scancodestr;
 }
-
-/* Get all the keys from the group model */
-static gchar** tilem_ksc_get_all_keys(const char* model) {
-
-	GKeyFile * gkf;
-	gchar** keys;
-	
-	gkf = g_key_file_new();
-	
-	if (! g_key_file_load_from_file(gkf, KEYBINDINGS_FILE, G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS, NULL)) {
-		fprintf(stderr, "Could not read config file '%s'\n", KEYBINDINGS_FILE);
-		exit(EXIT_FAILURE);
-	}
-
-	keys = g_key_file_get_keys(gkf, model, NULL, NULL);
-	
-//	for(i = 0; keys[i]; i++) 
-//		printf("keys %d : %s\n", i, keys[i]);
-		
-	g_key_file_free(gkf);
-	
-	return keys;
-}
-
-
