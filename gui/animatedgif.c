d0f3bde0cf4ebd9198609c880ea7f5bab92ea62f
contra-sh <contra-sh@05cf5367-e1a8-405a-8a5c-7f956f11530a>
2011-05-13 09:20:38 +0000

    Some code refactoring in animatedgif.c
    git-svn-id: svn://svn.code.sf.net/p/tilem/code/trunk@368 05cf5367-e1a8-405a-8a5c-7f956f11530a

--- gui/animatedgif.c
+++ gui/animatedgif.c
@@ -33,27 +33,35 @@ static void write_global_footer(FILE* fp);
 static void write_extension_block(FILE* fout, word delay);
 static void write_image_block_start(FILE *fp, int width, int height);
 static void write_image_block_end(FILE *fp);
+static void write_comment(FILE* fp);
+static void write_image_block(TilemCalcEmulator *emu, FILE *fp, int width, int height);
 
 void static_screenshot_save_with_parameters(TilemCalcEmulator* emu, char* filename, int width, int height) {
 	
-	TilemLCDBuffer * lcdbuf = tilem_lcd_buffer_new();
-	byte* buffer = g_new(byte, width * height);
-	tilem_lcd_get_frame(emu->calc, lcdbuf);
-	tilem_draw_lcd_image_indexed(lcdbuf, buffer, width, height, width, TILEM_SCALE_SMOOTH);
-
-
+	
 	printf("GIF ENCODER\n");
 	FILE *fp = fopen(filename, "w");
 	
 	write_global_header(fp, width, height);
 	write_extension_block(fp, 10);	
 	write_image_block_start(fp, width, height);	
-	GifEncode(fp, buffer , 8, (width*height));
+	write_image_block(emu, fp, width, height);
 	write_image_block_end(fp);
 	write_global_footer(fp);
 	fclose(fp);
 }
 
+static void write_image_block(TilemCalcEmulator * emu, FILE *fp, int width, int height) {
+
+	TilemLCDBuffer * lcdbuf = tilem_lcd_buffer_new();
+	byte* buffer = g_new(byte, width * height);
+	tilem_lcd_get_frame(emu->calc, lcdbuf);
+	tilem_draw_lcd_image_indexed(lcdbuf, buffer, width, height, width, TILEM_SCALE_SMOOTH);
+
+	GifEncode(fp, buffer , 8, (width*height));
+}
+
+	
 static void write_global_header(FILE* fp, int width, int height) {
 	
 	/* Magic number for Gif file format */
@@ -66,6 +74,14 @@ static void write_global_header(FILE* fp, int width, int height) {
 	global_header_canvas[2] = height; 
 
 	/* Flag */
+	/* The 11th byte is a set of flags  : 
+	bit 0:    Global Color Table Flag (GCTF)
+        bit 1..3: Color Resolution
+        bit 4:    Sort Flag to Global Color Table
+        bit 5..7: Size of Global Color Table: 2^(1+n)
+	It means "use the GCT wich is given after (from the size bit 5..7) and a resolution bit 1..3 
+	The Background color is an index in the Global Color Table
+	*/
     	char global_header_flag[] = { 0xf7 };
 	/* The index in global color table */
 	char global_header_background_index[] = {0x00};
@@ -143,30 +159,23 @@ static void write_image_block_end(FILE *fp) {
 	fwrite(image_block_end, 1, 1,fp);
 }
 
+static void write_comment(FILE* fp) {
 
+	char comment[] = {0x21, 0xfe, 8, 'T', 'i', 'l', 'E', 'm', '2', 0, 0, 0};
+	fwrite(comment, 12, 1, fp);
+}
 
 /* Create an empty gif and add the first frame */ 
 void tilem_animation_start(TilemCalcEmulator* emu) {
 	
-	/* The 11th byte is a set of flags  : 
-	bit 0:    Global Color Table Flag (GCTF)
-        bit 1..3: Color Resolution
-        bit 4:    Sort Flag to Global Color Table
-        bit 5..7: Size of Global Color Table: 2^(1+n)
-	It means "use the GCT wich is given after (from the size bit 5..7) and a resolution bit 1..3 
-	The Background color is an index in the Global Color Table
-	*/
 	
-	/* Magic number (GIF89a), width pixel 2 bytes, height pixel 2 bytes, GCT follows 256 values * 3 bytes (RGB) , bg color, default aspect ration */ 	
-	char gif_header[13] = {'G', 'I', 'F', '8', '9', 'a', 96, 0, 64, 0, 0xf7, 0x00, 0};
+
     	/* Introduce the block 3bytes, netscape (type of gif : animation), a new block comment */
 	char gif_infos[31] = {
         0x21, 0xff, 0x0b, 'N', 'E', 'T', 'S', 'C', 'A', 'P', 'E', '2', '.', '0', 3, 1, 0xff, 0xff, 0x00	};
 	
 	int width, height;
 	guchar* lcddata;
-	int k;
-	int x, y;
 	width = emu->calc->hw.lcdwidth;
 	height = emu->calc->hw.lcdheight;
 	
@@ -178,55 +187,23 @@ void tilem_animation_start(TilemCalcEmulator* emu) {
 
 
 
-
-
 	printf("GIF ENCODER\n");
 	FILE* fp;
 	fp = fopen("gifencod.gif", "w");
   	if(fp) { 
 			
-		/* TODO : convert this padding stuff to a real palette */ 
-		char palette_start[] = { 0x00, 0x00, 0x00};
-		char padding[] = { 0xff, 0xff, 0xff};
-		char palette_end[] = { 0xff, 0xff, 0xff};
-
 	    
 		/* Extension block introduced by 0x21 ('!'), and an img introduced by 0x2c (',') followed by coordinate corner(0,0), canvas 4 bytes, no local color table */
 		static char gif_img[18] = {0x21, 0xf9, 4, 5, 11, 0, 0x0f, 0, 0x2c, 0, 0, 0, 0, 96, 0, 64, 0, 0};
 	    
 		char end[1] = { 0x00};
 		
-		fwrite(gif_header, 13, 1, fp);
-		fwrite(palette_start, 3, 1, fp);
-		for(k = 0; k < 254; k++) {
-			fwrite(padding, 3, 1, fp);
-		}
-		fwrite(palette_end, 3, 1, fp);
+		write_global_header(fp, 96, 64)	;
 		fwrite(gif_infos, 19, 1, fp);
+		write_comment(fp);
 		fwrite(gif_img, 18, 1, fp);
 		
-		long i= 0;
-		
-		unsigned char q[(96*64)];
-
-
-		/* Reduce screen buffer to one byte per pixel */
-		for (y = 0; y < height; y++) {
-			for (x = 0; x < width; x++) {
-				if (lcddata[(y * width + x) / 8] & (0x80 >> (x % 8))) {
-					//printf("i = %d", i);
-					q[i] = 0x00;
-					i++;
-				} else {
-					q[i] = 0x02;
-					i++;
-				}
-			}
-		}	
-		
-		
-
-		GifEncode(fp, q , 1, (width*height));
+		write_image_block(emu, fp, width, height);
 		fwrite(end, 1, 1,fp);	/* Write end of the frame */
 		fclose(fp);
 		emu->gw->ss->isAnimScreenshotRecording = TRUE;
@@ -238,48 +215,16 @@ void tilem_animation_start(TilemCalcEmulator* emu) {
 void tilem_animation_add_frame(TilemCalcEmulator* emu) {
 	
 	printf("GIFENCODER addframe\n");
-	int width, height;
-	guchar* lcddata;
-	int x, y;
-	width = emu->calc->hw.lcdwidth;
-	height = emu->calc->hw.lcdheight;
-	/* Alloc mmem */
-	lcddata = g_new(guchar, (width / 8) * height);
 		
-	/* Get the lcd content using the function 's pointer from Benjamin's core */
-	(*emu->calc->hw.get_lcd)(emu->calc, lcddata);
-
+	int width = emu->calc->hw.lcdwidth;
+	int height = emu->calc->hw.lcdheight;
 
 	FILE* fp;
 	fp = fopen("gifencod.gif", "a");
 	if(fp) {
-    	
-    		/* Extension block introduced by 0x21 ('!'), and an img introduced by 0x2c (',') followed by coordinate corner(0,0), canvas 4 bytes, no local color table */
-		static char gif_img[18] = {0x21, 0xf9, 4, 8, 11, 0, 0x0f, 0, 0x2c, 0, 0, 0, 0, 96, 0, 64, 0, 0};
-		char end[1] = { 0x00};
-		
-		fwrite(gif_img, 18, 1, fp);
-		long i= 0;
-		
-		unsigned char q[(width * height)];
-
-		/* Reduce screen buffer to one byte per pixel */
-		for (y = 0; y < height; y++) {
-			for (x = 0; x < width; x++) {
-				if (lcddata[(y * width + x) / 8] & (0x80 >> (x % 8))) {
-					q[i] = 0x00;
-					i++;
-				} else {
-					q[i] = 0x02;
-					i++;
-				}
-			}
-		}	
-		
-		
-
-		GifEncode(fp, q , 1, (width*height));
-		fwrite(end, 1, 1,fp);
+		write_image_block_start(fp, width, height) ;
+		write_image_block(emu, fp, width, height);	
+		write_image_block_end(fp);
 		fclose(fp);
 	}
 }
@@ -287,12 +232,10 @@ void tilem_animation_add_frame(TilemCalcEmulator* emu) {
 /* Stop recording animations */
 void tilem_animation_stop(TilemCalcEmulator* emu) {
 	
-	
-    	char trailer[1] = { 0x3b};
 	if(emu->gw->ss->isAnimScreenshotRecording) {
 		FILE* fp;
 		fp = fopen("gifencod.gif", "a");
-		fwrite(trailer, 1, 1,fp);
+		write_global_footer(fp);
 		fclose(fp);
 		
 	}
