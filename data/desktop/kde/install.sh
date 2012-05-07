7971b07b79f4f3c99ec3ef28131157b39c74c280
contra-sh <contra-sh@05cf5367-e1a8-405a-8a5c-7f956f11530a>
2012-05-07 15:10:41 +0000

    Some update on .desktop. Not usable yet
    git-svn-id: svn://svn.code.sf.net/p/tilem/code/trunk@616 05cf5367-e1a8-405a-8a5c-7f956f11530a

---
+++ data/desktop/kde/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Script taken from TiEmu 
+
+echo "Beware: script intented to developers: unsupported!"
+cp tilem2.desktop /usr/share/applications
+cp x-tilem2-*.desktop /usr/share/mimelnk/application
+echo "Files copied."
