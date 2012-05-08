8d9336267586288ec1b65fdf78be775b3247c7b1
floppusmaximus <floppusmaximus@05cf5367-e1a8-405a-8a5c-7f956f11530a>
2012-05-08 00:45:59 +0000

    Convert desktop file to current standard format.
    We'll want MIME type definitions, too; I'm working on those.  I
    haven't found any standard names for these MIME types, so I'm making
    them up.
    
    
    git-svn-id: svn://svn.code.sf.net/p/tilem/code/trunk@617 05cf5367-e1a8-405a-8a5c-7f956f11530a

--- data/desktop/kde/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Script taken from TiEmu 
-
-echo "Beware: script intented to developers: unsupported!"
-cp tilem2.desktop /usr/share/applications
-cp x-tilem2-*.desktop /usr/share/mimelnk/application
-echo "Files copied."
